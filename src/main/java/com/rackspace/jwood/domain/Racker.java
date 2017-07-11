06288b946fe3c8d0be86b3e31380e2ef2576b7ac
Justin Wood <root@justinwood.info>
2017-07-11 13:00:31 -0500

    demo

--- src/main/java/com/rackspace/jwood/domain/Racker.java
+++ src/main/java/com/rackspace/jwood/domain/Racker.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class Racker {
+	private Integer rackerId;
+
 	@NotEmpty
 	private String name;
 	
@@ -13,7 +15,13 @@ public class Racker {
 	private String title;
 
 	private List<String> strengths = new LinkedList<String>();
-	
+
+	public Integer getId() {
+		return rackerId;
+	}
+	public void setId(Integer rackerId) {
+		this.rackerId = rackerId;
+	}
 	public String getName() {
 		return name;
 	}
